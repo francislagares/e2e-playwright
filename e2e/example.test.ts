0def54846ab1b5f960b20f89a62a81233ac77917
Francis Lagares <francis.lagares@gmail.com>
2022-03-01 21:52:07 +0100

    feat: initial project setup

---
+++ e2e/example.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('Simple starter Test', async ({ page }) => {
+  await page.goto('https://example.com');
+
+  const pageTitle = await page.locator('h1');
+
+  await expect(pageTitle).toContainText('Example Domain');
+});
