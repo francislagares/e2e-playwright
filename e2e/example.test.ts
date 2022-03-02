ba59b11502276b9be1b8942ca4aa2de5ad4177cf
Francis Lagares <francis.lagares@gmail.com>
2022-03-02 21:33:08 +0100

    test(e2e): working with inputs

--- e2e/example.test.ts
+++ e2e/example.test.ts
@@ -10,12 +10,24 @@ test('Simple starter Test', async ({ page }) => {
 
 test('Click on Elements', async ({ page }) => {
   await page.goto('http://zero.webappsecurity.com/');
-
   await page.click('#signin_button');
 
   await page.click('text=Sign in');
 
-  const errorMessage = await page.locator('.alert-error');
+  const errorMessage = page.locator('.alert-error');
+
+  await expect(errorMessage).toContainText('Login and/or password are wrong.');
+});
+
+test('Working with Input', async ({ page }) => {
+  await page.goto('http://zero.webappsecurity.com/');
+  await page.click('#signin_button');
+
+  await page.type('#user_login', 'johndoe');
+  await page.type('#user_password', 'password');
+  await page.click('text=Sign in');
+
+  const errorMessage = page.locator('.alert-error');
 
   await expect(errorMessage).toContainText('Login and/or password are wrong.');
 });
