d377138c7982e06bdf2b209262ab3094d64551e4
Francis Lagares <francis.lagares@gmail.com>
2022-03-02 20:43:25 +0100

    test(e2e): click on elements

--- e2e/example.test.ts
+++ e2e/example.test.ts
@@ -3,7 +3,19 @@ import { test, expect } from '@playwright/test';
 test('Simple starter Test', async ({ page }) => {
   await page.goto('https://example.com');
 
-  const pageTitle = await page.locator('h1');
+  const pageTitle = page.locator('h1');
 
   await expect(pageTitle).toContainText('Example Domain');
+});
+
+test('Click on Elements', async ({ page }) => {
+  await page.goto('http://zero.webappsecurity.com/');
+
+  await page.click('#signin_button');
+
+  await page.click('text=Sign in');
+
+  const errorMessage = await page.locator('.alert-error');
+
+  await expect(errorMessage).toContainText('Login and/or password are wrong.');
 });
