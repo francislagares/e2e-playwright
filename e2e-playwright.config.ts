d9760b18772693cd82d15edb7d9bd794931b0b91
Francis Lagares <francis.lagares@gmail.com>
2022-03-05 19:10:17 +0100

    chore: add e2e config file

---
+++ e2e-playwright.config.ts
@@ -0,0 +1,35 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  timeout: 40000,
+  retries: 0,
+  testDir: 'tests/e2e',
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 5000,
+    ignoreHTTPSErrors: true,
+    video: 'off',
+    screenshot: 'off',
+  },
+  projects: [
+    {
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'Firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'Webkit',
+      use: { browserName: 'webkit' },
+    },
+  ],
+  reporter: [
+    [process.env.CI ? 'github' : 'list'],
+    ['html', { outputFolder: './tests/e2e/reports', open: 'never' }],
+  ],
+};
+
+export default config;
