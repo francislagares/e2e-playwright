839797060c524f0f332c8b3807a35d608ce9181a
Francis Lagares <francis.lagares@gmail.com>
2022-03-07 12:17:47 +0100

    test(api): test user GET request

--- tests/api/api.test.ts
+++ tests/api/api.test.ts
@@ -6,10 +6,23 @@ test.describe.parallel('API Testing', () => {
   test('Simple API Test - Assert Response Status', async ({ request }) => {
     const response = await request.get(`${baseUrl}/users/2`);
     expect(response.status()).toBe(200);
+
+    const responseBody = JSON.parse(await response.text());
   });
 
   test('Simple API Test - Assert Invalid Endpoint', async ({ request }) => {
     const response = await request.get(`${baseUrl}/users/non-existing-enpoint`);
     expect(response.status()).toBe(404);
+  });
+
+  test('Get Request - Get User Detail', async ({ request }) => {
+    const response = await request.get(`${baseUrl}/users/1`);
+    const responseBody = JSON.parse(await response.text());
+
+    expect(response.status()).toBe(200);
+    expect(responseBody.data.id).toBe(1);
+    expect(responseBody.data.first_name).toBe('George');
+    expect(responseBody.data.last_name).toBe('Bluth');
+    expect(responseBody.data.email).toBeTruthy();
   });
 });
